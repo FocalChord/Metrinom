ce51eae221c607cb0623e23a607db837b3c0cc6a
Nisarag <nisaragb@gmail.com>
2020-03-23 12:06:24 +1300

    adding initial client with react installed

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className="text-center">
+            <header className="min-h-screen flex flex-col items-center justify-center bg-black text-white text-6xl">
+                Spotify
+            </header>
+        </div>
+    );
+};
+
+export default App;
