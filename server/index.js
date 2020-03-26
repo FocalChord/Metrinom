79d43d78cbcf16e1d334943b580a5be309c9ce17
Hong <37763078+HongShi10@users.noreply.github.com>
2020-03-26 18:15:30 +1300

    created .env.example file and mongodb database connection (#10)
    Co-authored-by: Hongye Shi <hongye@1007@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
+
+const connectionString = process.env.DEV_DB_CONN || "mongodb://localhost/Metrinome";
+// Connect to Mongoose
+mongoose.connect(connectionString, { useNewUrlParser: true });
+const db = mongoose.connection;
+
+// Checking for DB connection
+db.once("open", () => {
+    console.log("Connected to MongoDB.");
+});
+db.on("error", () => {});
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,7 +24,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello Worldsss");
 });
-
+// app.get("/api/users", (req, res) => {
+//     res.send;
+// });
 app.listen(port, () => console.log(`Server running on port ${port}`));
