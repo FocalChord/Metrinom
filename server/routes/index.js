7bb20594279c1052c57d2933d12a2adcff5944a7
Hong <hongye1007@gmail.com>
2020-03-30 16:54:28 +1300

    removed comment

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,5 @@ const router = express.Router();
 
 router.use("/auth", require("./auth"));
 router.use("/user", require("./user-route"));
-// router.use("/swagger", require("./swagger"));
 
 module.exports = router;
