376257fb3eaea312dd54e8596da32326bc71bfb6
Hong <hongye1007@gmail.com>
2020-03-30 13:34:38 +1300

    added update and get single methods

--- server/index.js
+++ server/index.js
@@ -8,16 +8,4 @@ const startServer = async () => {
     app.listen(port, () => console.log(`Server running on port ${port}`));
 };
 
-<<<<<<< HEAD
 startServer();
-=======
-app.get("/", (req, res) => {
-    res.send("Hello Worldsss");
-});
-
-const user = require("./src/routing/user-routing");
-
-app.use("/user", user);
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
->>>>>>> added get ALL method for users
--- server/src/routing/user-routing.js
+++ server/src/routing/user-routing.js
@@ -3,17 +3,43 @@ const express = require("express");
 const router = express.Router();
 
 const User = require("../models/user");
-// require("../log/logger");
+// import logger from "../log/logger";
 
-// GET All users in DATABASE
-router.get("/getAll", (req, res) => {
-    User.find().then((users, err) => {
+// GET Single user
+router.get("/:id", (req, res) => {
+    User.findOne({ spotifyUserId: req.params.id }, (err, users) => {
         if (err) {
             // logger.error(err);
-            res.status(400).json({ msg: "lol" });
+            res.status(400).json({ msg: "error" });
         } else {
             // logger.info(users);
             res.status(200).json(users);
+        }
+    });
+});
+
+// GET All users in DATABASE
+router.get("/", (req, res) => {
+    User.find({}, (err, users) => {
+        if (err) {
+            // logger.error(err);
+            res.status(400).json({ msg: err });
+        } else {
+            // logger.info(users);
+            res.status(200).json(users);
+        }
+    });
+});
+
+// Updates a users information
+router.put("/:id", (req, res) => {
+    User.updateOne({ spotifyUserId: req.params.id }, req.body, { upsert: "true" }, (err, user) => {
+        if (err) {
+            // logger.error(err);
+            res.status(400).json({ msg: "error" });
+        } else {
+            // logger.info(users);
+            res.status(200).json(user);
         }
     });
 });
