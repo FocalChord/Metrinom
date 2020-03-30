d8c6d379a5569af5199ea2bf9c57cc85e1327856
Hong <hongye1007@gmail.com>
2020-03-30 15:33:44 +1300

    added swagger documentation

--- server/src/routing/user-routing.js
+++ server/src/routing/user-routing.js
@@ -6,6 +6,29 @@ const User = require("../models/user");
 // import logger from "../log/logger";
 
 // GET Single user
+/**
+ * @swagger
+ * path:
+ *  /user/{id}:
+ *    get:
+ *      tags: [Users]
+ *      summary: Get a user by spotify id
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: Id of the user
+ *      responses:
+ *        "200":
+ *          description: An array of users object
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ *
+ */
 router.get("/:id", (req, res) => {
     User.findOne({ spotifyUserId: req.params.id }, (err, users) => {
         if (err) {
@@ -18,7 +41,21 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// GET All users in DATABASE
+/**
+ * @swagger
+ * path:
+ *  /user/:
+ *    get:
+ *      tags: [Users]
+ *      summary: Get all the users
+ *      responses:
+ *        "200":
+ *          description: An array of users object
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ */
 router.get("/", (req, res) => {
     User.find({}, (err, users) => {
         if (err) {
@@ -30,10 +67,30 @@ router.get("/", (req, res) => {
         }
     });
 });
-
 // Updates a users information
-router.put("/:id", (req, res) => {
-    User.updateOne({ spotifyUserId: req.params.id }, req.body, { upsert: "true" }, (err, user) => {
+/**
+ * @swagger
+ * path:
+ *  /user/:
+ *    put:
+ *      tags: [Users]
+ *      summary: Updates a user
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      responses:
+ *        "200":
+ *          description: A user schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ */
+router.put("/", (req, res) => {
+    User.updateOne({ spotifyUserId: req.body.spotifyUserId }, req.body, { upsert: "true" }, (err, user) => {
         if (err) {
             // logger.error(err);
             res.status(400).json({ msg: "error" });
