242f90a08a2cb6eea9c69313dd387ed4d06558cd
Hongye Shi <hongye@1007@gmail.com>
2020-04-04 15:30:49 +1300

    added route to get top tracks and artsits for a user

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use("/auth", require("./auth"));
 router.use("/user", require("./user-route"));
 router.use("/notification", require("./notification-route"));
+router.use("/spotify", require("./spotify-route"));
 
 module.exports = router;
