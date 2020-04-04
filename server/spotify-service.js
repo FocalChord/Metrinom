ca7f0b79b5184a3dd8cc18a9c50a04e10e795100
Hongye Shi <hongye@1007@gmail.com>
2020-04-04 22:02:50 +1300

    added try catch to fetch statemets

--- server/spotify-service.js
+++ server/spotify-service.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const spotify = require("./spotify-logic");
+const LOGGER = require("../log/logger");
 
 const fetchTopArtistOrTracks = async (url, type, timeFrame, authToken) => {
     const headers = {
@@ -7,9 +8,13 @@ const fetchTopArtistOrTracks = async (url, type, timeFrame, authToken) => {
         Accept: "application/json",
         "Content-Type": "application/json",
     };
-    const response = await fetch(url + `/${type}?time_range=${timeFrame}&limit=50`, { method: "GET", headers: headers });
-    const json = await response.json();
-    return json;
+    try {
+        const response = await fetch(url + `/${type}?time_range=${timeFrame}&limit=50`, { method: "GET", headers: headers });
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        LOGGER.error(error);
+    }
 };
 
 const fetchRecomendations = async (url, seedArtist, seedTracks, seedGenres, authToken) => {
@@ -18,13 +23,18 @@ const fetchRecomendations = async (url, seedArtist, seedTracks, seedGenres, auth
         Accept: "application/json",
         "Content-Type": "application/json",
     };
-    const response = await fetch(
-        url + `/?limit=20&seed_artist=${seedArtist}&seed_tracks=${seedTracks}&seed_genres=${seedGenres}&min_energy=0.4&min_popularity=50`,
-        { method: "GET", headers: headers },
-    );
-    const json = await response.json();
+    try {
+        const response = await fetch(
+            url +
+                `/?limit=20&seed_artist=${seedArtist}&seed_tracks=${seedTracks}&seed_genres=${seedGenres}&min_energy=0.4&min_popularity=50`,
+            { method: "GET", headers: headers },
+        );
+        const json = await response.json();
 
-    return json;
+        return json;
+    } catch (error) {
+        LOGGER.error(error);
+    }
 };
 
 const fetchTopGenres = async (url, timeFrame, authToken) => {
@@ -33,11 +43,15 @@ const fetchTopGenres = async (url, timeFrame, authToken) => {
         Accept: "application/json",
         "Content-Type": "application/json",
     };
-    const response = await fetch(url + `/artists?time_range=${timeFrame}&limit=50`, { method: "GET", headers: headers });
-    const json = await response.json();
-    const topGenreJson = spotify.findGenres(json);
+    try {
+        const response = await fetch(url + `/artists?time_range=${timeFrame}&limit=50`, { method: "GET", headers: headers });
+        const json = await response.json();
+        const topGenreJson = spotify.findGenres(json);
 
-    return topGenreJson;
+        return topGenreJson;
+    } catch (error) {
+        LOGGER.error(error);
+    }
 };
 
 module.exports = { fetchTopArtistOrTracks, fetchRecomendations, fetchTopGenres };
