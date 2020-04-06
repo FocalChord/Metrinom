19707152b42556ce3267e09e4846fc65c1d5514b
Nisarag <nisaragb@gmail.com>
2020-04-07 00:08:58 +1200

    adding boilerplate page for routing components

---
+++ client/src/components/RedirectPage/RedirectPage.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { Redirect, useParams } from "react-router-dom";
+import CookieManager from "../../utils/CookieManager";
+
+const RedirectPage = () => {
+    const [redirect, setRedirect] = useState(false);
+    const params = useParams();
+
+    useEffect(() => {
+        const { token } = params;
+        CookieManager.setUserToken(token);
+        setRedirect(true);
+    }, []);
+
+    if (redirect) {
+        return <Redirect to="/" />;
+    }
+
+    return (
+        <div className="text-center">
+            <header className="min-h-screen flex flex-col items-center justify-center bg-black text-white text-6xl">Redirect</header>
+        </div>
+    );
+};
+
+export default RedirectPage;
