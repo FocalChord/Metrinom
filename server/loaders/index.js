e6fc803875670e37fa9ab12ff41d7bbf5ac62fa8
Nisarag <nisaragb@gmail.com>
2020-04-13 21:33:12 +1200

    adding middleware with winston so each request is now logged automatically

--- server/loaders/index.js
+++ server/loaders/index.js
@@ -1,9 +1,11 @@
 const expressLoader = require("./express");
+const loggerLoader = require("./logger");
 const mongooseLoader = require("./mongoose");
 const passportLoader = require("./passport");
 const swaggerLoader = require("./swagger");
 
 module.exports = async (app) => {
+    loggerLoader(app);
     await mongooseLoader();
     passportLoader(app);
     expressLoader(app);
