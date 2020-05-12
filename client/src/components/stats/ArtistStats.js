206210f1d2a7fde7f49fa5f8492fe956b6bcd5e3
Hong <hongye1007@gmail.com>
2020-05-12 15:18:02 +1200

    added styling to each artist

--- client/src/components/stats/ArtistStats.js
+++ client/src/components/stats/ArtistStats.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Card, Box, CardMedia, CardContent, Typography } from "@material-ui/core";
+import { Card, CardMedia, CardContent, Typography, Grid, CardActionArea } from "@material-ui/core";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 import { MetrinomContext } from "../../context/MetrinomContext";
@@ -17,13 +17,58 @@ const ArtistStats = () => {
             setIsLoading(false);
         });
     }, []);
-    const theme = createMuiTheme({
-        palette: {
-            primary: purple,
-            secondary: green,
-        },
-        status: {
-            danger: "orange",
+    const muiBaseTheme = createMuiTheme();
+
+    const getTheme = (muiBaseTheme) => ({
+        MuiCard: {
+            root: {
+                "&.MuiPostCard--02": {
+                    borderRadius: muiBaseTheme.spacing.unit * 2, // 16px
+                    transition: "0.3s",
+                    boxShadow: "0px 14px 80px rgba(34, 35, 58, 0.2)",
+                    background: "#424242",
+                    marginLeft: "auto",
+                    overflow: "initial",
+                    width: "100%",
+                    maxWidth: 300,
+                    padding: `${muiBaseTheme.spacing.unit * 2}px 0`,
+                    "&:hover": {
+                        transform: "translateY(-7px)",
+                        boxShadow: "0 4px 20px 0 #1DB954",
+                    },
+
+                    "& .MuiCardMedia-root": {
+                        flexShrink: 0,
+                        width: "55%",
+                        height: "55%",
+                        paddingTop: "55%",
+                        transform: "translateX(-24%)",
+                        borderRadius: muiBaseTheme.spacing.unit * 2, // 16
+                        overflow: "hidden",
+                    },
+                    "& .MuiCardContent-root": {
+                        textAlign: "left",
+                        "text-overflow": "ellipsis",
+                        "white-space": "nowrap",
+                        overflow: "hidden",
+                        paddingLeft: 0,
+                        padding: muiBaseTheme.spacing.unit * 2,
+                    },
+                    "& .MuiTypography--heading": {
+                        color: "white",
+                    },
+                    "& .MuiTypography--subheading": {
+                        marginBottom: muiBaseTheme.spacing.unit * 2,
+                        color: "grey",
+                    },
+                    "& .MuiCardContent--actionarea": {
+                        display: "flex",
+                        "justify-content": "flex-start",
+
+                        outline: "none",
+                    },
+                },
+            },
         },
     });
     return (
@@ -31,33 +76,37 @@ const ArtistStats = () => {
             <div className="text-center">
                 <h1>Your top Artists are</h1>
                 <ul>
-                    {artists ? (
-                        artists.map((artist, idx) => (
-                            <Box key={artist.trackId}>
-                                <MuiThemeProvider
-                                    theme={createMuiTheme({
-                                        typography: {
-                                            useNextVariants: true,
-                                        },
-                                        overrides: theme,
-                                    })}
-                                >
-                                    <Card className={"MuiPostCard--02"}>
-                                        <CardMedia className={"MuiCardMedia-root"} image={artist.images[0].url} />
-                                        <CardContent className={"MuiCardContent-root"}>
-                                            <Typography className={"MuiTypography--heading"} variant={"h6"} gutterBottom>
-                                                {artist.name}
-                                            </Typography>
-                                            <Typography className={"MuiTypography--subheading"}>{artist.genres[0]}</Typography>
-                                            <Typography className={"MuiTypography--subheading"}>{idx + 1}</Typography>
-                                        </CardContent>
-                                    </Card>
-                                </MuiThemeProvider>
-                            </Box>
-                        ))
-                    ) : (
-                        <li>No Artist data available :(</li>
-                    )}
+                    <Grid container spacing={3}>
+                        {artists ? (
+                            artists.map((artist, idx) => (
+                                <Grid item key={artist.id} xs={12} sm={10} md={3}>
+                                    <MuiThemeProvider
+                                        theme={createMuiTheme({
+                                            typography: {
+                                                useNextVariants: true,
+                                            },
+                                            overrides: getTheme(muiBaseTheme),
+                                        })}
+                                    >
+                                        <Card className={"MuiPostCard--02"}>
+                                            <CardActionArea className={"MuiCardContent--actionarea"}>
+                                                <CardMedia className={"MuiCardMedia-root"} image={artist.images[0].url} />
+                                                <CardContent className={"MuiCardContent-root"}>
+                                                    <Typography className={"MuiTypography--heading"} variant={"h6"} gutterBottom>
+                                                        {artist.name}
+                                                    </Typography>
+                                                    <Typography className={"MuiTypography--subheading"}>{artist.genres[0]}</Typography>
+                                                    <Typography className={"MuiTypography--subheading"}>#{idx + 1}</Typography>
+                                                </CardContent>
+                                            </CardActionArea>
+                                        </Card>
+                                    </MuiThemeProvider>
+                                </Grid>
+                            ))
+                        ) : (
+                            <li>No Artist data available :(</li>
+                        )}
+                    </Grid>
                 </ul>
             </div>
         </LoaderWrapper>
