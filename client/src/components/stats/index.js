04a7c69fb3871b170dcf03d5aec663161f7d8a5d
Nisarag <nisaragb@gmail.com>
2020-05-22 14:41:13 +1200

    removing files which are not being used

--- client/src/components/stats/index.js
+++ client/src/components/stats/index.js
@@ -1,6 +1,5 @@
 import ArtistStats from "./ArtistStats";
 import GenreStats from "./GenreStats";
-import StatsHeader from "./StatsHeader";
 import TrackStats from "./TrackStats";
 
-export { StatsHeader, GenreStats, ArtistStats, TrackStats };
+export { GenreStats, ArtistStats, TrackStats };
