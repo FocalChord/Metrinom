6431fc9eeb14d40d1edbfd0920ad9a1077dc22f4
Hong <hongye1007@gmail.com>
2020-05-24 14:05:13 +1200

    changed spotify auth scopes to only necessary ones

--- server/configs/spotify-auth-scopes.js
+++ server/configs/spotify-auth-scopes.js
@@ -1,10 +1,8 @@
-const spotifyConnect = ["user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing"];
 const users = ["user-read-email", "user-read-private"];
-const playlists = ["playlist-read-collaborative", "playlist-modify-public", "playlist-read-private", "playlist-modify-private"];
-const library = ["user-library-modify", "user-library-read"];
-const libraryHistory = ["user-top-read", "user-read-playback-position", "user-read-recently-played"];
+const playlists = ["playlist-modify-public", "playlist-modify-private"];
+const libraryHistory = ["user-top-read", "user-read-recently-played"];
 const follow = ["user-follow-modify", "user-follow-read"];
 
-const spotifyAuthScope = [...spotifyConnect, ...users, ...playlists, ...library, ...libraryHistory, ...follow];
+const spotifyAuthScope = [...users, ...playlists, ...libraryHistory, ...follow];
 
 module.exports = spotifyAuthScope;
