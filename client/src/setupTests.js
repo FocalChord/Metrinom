2934b0533f62872ca6260c40eb2e3a1c31f50a21
dinith1 <Dinith1@users.noreply.github.com>
2020-05-24 13:45:28 +1200

    rebase with master

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+// import { configure } from "enzyme-adapter-react-16";
+// configure({ adapter: new Adapter() });
+// import "jest-enzyme";
+
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
