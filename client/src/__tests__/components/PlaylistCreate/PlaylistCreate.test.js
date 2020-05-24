e07ef59b211cf0f113a9ca81f7f0e6d89ac9d366
dinith1 <Dinith1@users.noreply.github.com>
2020-05-24 14:06:22 +1200

    add headerbar test

--- client/src/__tests__/components/PlaylistCreate/PlaylistCreate.test.js
+++ client/src/__tests__/components/PlaylistCreate/PlaylistCreate.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import "regenerator-runtime/runtime.js";
-
 import PlaylistCreate from "../../../components/PlaylistCreate";
 
 test("<PlaylistCreate />", () => {
--- client/src/__tests__/components/PrivateRoute/HeaderBar.test.js
+++ client/src/__tests__/components/PrivateRoute/HeaderBar.test.js
@@ -1,19 +1,10 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import "regenerator-runtime/runtime.js";
-import { Route } from "react-router-dom";
-
 import HeaderBar from "../../../components/PrivateRoute/Border/HeaderBar";
 
-const routes = [{ name: "Home", path: "/", icon: <div /> }];
-
 test("<HeaderBar />", () => {
-    // const tree = renderer
-    //     .create(
-    //         <Route>
-    //             <HeaderBar routes={routes} />
-    //         </Route>,
-    //     )
-    //     .toJSON();
-    // expect(tree).toMatchSnapshot();
+    const tree = renderer.create(<HeaderBar />).toJSON();
+
+    expect(tree).toMatchSnapshot();
 });
